dde6e9d7c03d21444f273d4da68c7a36ef9367fe
PRAKASH THAPA <prakasambhav24@gmail.com>
2024-11-27 13:07:28 +0545

    frontend of logic of post is done...

---
+++ frontend/src/components/CommentDialog.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import { Link } from "react-router-dom";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { MoreHorizontal } from "lucide-react";
+import { Button } from "./ui/button";
+
+const CommentDialog = ({ open, setOpen }) => {
+  const [text, setText] = useState("");
+  const changeEventHandler = (e) => {
+    if (e.target.value.trim()) {
+      setText(e.target.value);
+    } else {
+      setText("");
+    }
+  };
+  const sendEventHandler = async () => {
+    alert(text);
+  };
+  return (
+    <Dialog open={open}>
+      <DialogContent
+        onInteractOutside={() => setOpen(false)}
+        className="max-w-4xl p-0 flex flex-col"
+      >
+        <div className="flex flex-1">
+          <div className="w-1/2">
+            <img
+              src="https://images.unsplash.com/photo-1732444827849-f0da10c4122c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D"
+              alt="post_img"
+              className="w-full h-full object-cover rounded-l-lg"
+            />
+          </div>
+          <div className="w-1/2 flex flex-col justify-between">
+            <div className="flex item-center justify-between p-4">
+              <div className="flex gap-3 items-center">
+                <Link>
+                  <Avatar>
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                </Link>
+                <div>
+                  <Link className="font-semibold text-xs">Username</Link>
+                  {/* <span className="text-gary-600 text-sm">Bio here...</span> */}
+                </div>
+              </div>
+
+              <Dialog>
+                <DialogTrigger asChild>
+                  <MoreHorizontal className="cursor-pointer" />
+                </DialogTrigger>
+                <DialogContent className="flex flex-col items-center text-sm text-center">
+                  <div className="cursor-pointer w-full text-[#ED4956] font-bold">
+                    Unfollow
+                  </div>
+                  <div className="cursor-pointer w-full">Add to favorites</div>
+                </DialogContent>
+              </Dialog>
+            </div>
+            <hr />
+            <div className="flex-1 overflow-y-auto max-h-100 p-4">
+              comments here
+            </div>
+            <div className="p-4">
+              <div className="flex items-center gap-2">
+                <input
+                  type="text"
+                  value={text}
+                  onChange={changeEventHandler}
+                  placeholder="Add a comment..."
+                  className="w-full outline-none border border-gray-300 p-2 rounded"
+                />
+                <Button
+                  disabled={!text.trim()}
+                  onClick={sendEventHandler}
+                  variant="outline"
+                >
+                  Post
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CommentDialog;
--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Bookmark, MessageCircle, MoreHorizontal, Send } from "lucide-react";
+import { Button } from "./ui/button";
+import { FaHeart } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa";
+import CommentDialog from "./CommentDialog";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 
 const Post = () => {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+  const changeEventHandler = (e) => {
+    if (e.target.value.trim()) {
+      setText(e.target.value);
+    } else {
+      setText("");
+    }
+  };
   return (
     <div className="my-8 w-full  max-w-sm mx-auto">
       <div className="flex items-center justify-between">
@@ -12,6 +27,71 @@ const Post = () => {
           </Avatar>
           <h1>Username</h1>
         </div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <MoreHorizontal className="cursor-pointer" />
+          </DialogTrigger>
+          <DialogContent className="flex flex-col items-center text-sm text-center">
+            <Button
+              variant="ghost"
+              className="cursor-pointer w-fit text-[#ED4956] font-bold"
+            >
+              Unfollow
+            </Button>
+            <Button variant="ghost" className="cursor-pointer w-fit">
+              Add to favorites
+            </Button>
+            <Button variant="ghost" className="cursor-pointer w-fit">
+              Delete
+            </Button>
+          </DialogContent>
+        </Dialog>
+      </div>
+      <img
+        className="rounded-sm my-2 w-full aspect-square object-cover"
+        src="https://images.unsplash.com/photo-1732444827849-f0da10c4122c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D"
+        alt="post_img"
+      />
+      <div className="flex items-center justify-between my-2">
+        <div className="flex items-center gap-3">
+          <FaRegHeart
+            size={"22px"}
+            className="cursor-pointer hover:text-gray-600"
+          />
+          <MessageCircle
+            onClick={() => {
+              setOpen(true);
+            }}
+            className="cursor-pointer hover:text-gray-600"
+          />
+          <Send className="cursor-pointer hover:text-gray-600" />
+        </div>
+        <Bookmark className="cursor-pointer hover:text-gray-600" />
+      </div>
+
+      <span className="font-medium block mb-2">1k likes</span>
+      <p>
+        <span className="font-medium mr-2">username</span>
+        caption
+      </p>
+      <span
+        onClick={() => {
+          setOpen(true);
+        }}
+        className="cursor-pointer text-sm text-gray-400"
+      >
+        View all 10 comments
+      </span>
+      <CommentDialog open={open} setOpen={setOpen} />
+      <div className="flex items-center justify-between">
+        <input
+          type="text"
+          value={text}
+          onChange={changeEventHandler}
+          placeholder="Add a comment..."
+          className="outline-none text-sm w-full"
+        />
+        {text && <span className="text-[#3BADF8]">Post</span>}
       </div>
     </div>
   );
